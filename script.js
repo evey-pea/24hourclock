2f802bcf5e7784cc3fdc3b5773b198acf838508e
Ross Baker <rossbaker32@gmail.com>
2020-04-07 15:15:12 +1000

    Modified equation for calculation hand ratios needed to add 12 to the hours ratio to account for the orientation of the clock face plus a couple of other small modifications to the equations

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const secondHand = document.querySelector('[data-second-hand]')
 function setClock() {
   const currentDate = new Date()
   const secondsRatio = currentDate.getSeconds() / 60
-  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
-  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  const minutesRatio = currentDate.getMinutes() / 60
+  const hoursRatio = (currentDate.getHours() + 12) / 24
   setRotation(secondHand, secondsRatio)
   setRotation(minuteHand, minutesRatio)
   setRotation(hourHand, hoursRatio)
